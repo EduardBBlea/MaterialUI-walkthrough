5aaeac1c2eb0a661d69fe1c72f80f800fe108c39
Eduard B <edd.bba@gmail.com>
2022-07-24 17:42:20 +0100

    covered button styiling including buttonGroup

--- src/components/MuiButton.jsx
+++ src/components/MuiButton.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-import {Stack, Button, IconButton} from '@mui/material'
+import {Stack, Button, IconButton, ButtonGroup} from '@mui/material'
 import SendIcon from '@mui/icons-material/Send';
 
 const MuiButton = () => {
   return (
     <Stack spacing={4}>
         <Stack spacing={2} direction="row">
+            {/* Button variants style */}
             <Button variant='text' href='https://google.com'>Text</Button>
             <Button variant='contained'>Contained</Button>
             <Button variant='outlined'>Outlined</Button>
         </Stack>
         <Stack spacing={2} direction="row">
+            {/* Button colors style */}
             <Button variant='contained' color='primary'>Primary</Button>
             <Button variant='contained' color='secondary'>Secondary</Button>
             <Button variant='text' color='error'>Error</Button>
@@ -20,16 +22,33 @@ const MuiButton = () => {
             <Button variant='contained' color='success'>Success</Button>
         </Stack>
         <Stack display="block" spacing={2} direction="row">
+            {/* Button size style */}
             <Button variant='contained' size='small'>Small</Button>
             <Button variant='contained' size='medium'>Medium</Button>
             <Button variant='contained' size='large'>Large</Button>
         </Stack>
         <Stack spacing={2} direction="row">
+            {/* Button with icons style */}
             <Button variant='contained' startIcon={<SendIcon/>} disableRipple onClick={()=>alert("HI")}>Send</Button>
             <Button variant='contained' endIcon={<SendIcon/>} disableElevation>Send</Button>
+            {/* Icon buttons  */}
             <IconButton aria-label='send' color="success" size="small">
                 <SendIcon/>
             </IconButton>
+        </Stack>
+        <Stack direction="row">
+            <ButtonGroup 
+            variant='outlined' 
+            orientation='vertical' 
+            size='small' 
+            color='secondary' 
+            aria-label='alignment button group'
+            >
+                <Button onClick={()=>alert("Hello, world!")}>Left</Button> 
+                <Button>Center</Button> 
+                <Button>Right</Button>
+            </ButtonGroup>
+             
         </Stack>
     </Stack>
         
