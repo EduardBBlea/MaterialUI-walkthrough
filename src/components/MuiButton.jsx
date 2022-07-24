cfd1ce846d21dd0e036e64f7cea096b2701c9482
Eduard B <edd.bba@gmail.com>
2022-07-24 18:09:59 +0100

    toggle button group

--- src/components/MuiButton.jsx
+++ src/components/MuiButton.jsx
@@ -1,59 +1,128 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import {Stack, Button, IconButton, ButtonGroup} from '@mui/material'
-import SendIcon from '@mui/icons-material/Send';
+import {
+  Stack,
+  Button,
+  IconButton,
+  ButtonGroup,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
 
 const MuiButton = () => {
+  const [formats, setFormats] = useState(null);
+  console.log(formats);
+  const handleFormatChange = (event, updatedFormats) => {
+    setFormats(updatedFormats);
+  };
   return (
     <Stack spacing={4}>
-        <Stack spacing={2} direction="row">
-            {/* Button variants style */}
-            <Button variant='text' href='https://google.com'>Text</Button>
-            <Button variant='contained'>Contained</Button>
-            <Button variant='outlined'>Outlined</Button>
-        </Stack>
-        <Stack spacing={2} direction="row">
-            {/* Button colors style */}
-            <Button variant='contained' color='primary'>Primary</Button>
-            <Button variant='contained' color='secondary'>Secondary</Button>
-            <Button variant='text' color='error'>Error</Button>
-            <Button variant='outlined' color='warning'>Warning</Button>
-            <Button variant='contained' color='info'>Info</Button>
-            <Button variant='contained' color='success'>Success</Button>
-        </Stack>
-        <Stack display="block" spacing={2} direction="row">
-            {/* Button size style */}
-            <Button variant='contained' size='small'>Small</Button>
-            <Button variant='contained' size='medium'>Medium</Button>
-            <Button variant='contained' size='large'>Large</Button>
-        </Stack>
-        <Stack spacing={2} direction="row">
-            {/* Button with icons style */}
-            <Button variant='contained' startIcon={<SendIcon/>} disableRipple onClick={()=>alert("HI")}>Send</Button>
-            <Button variant='contained' endIcon={<SendIcon/>} disableElevation>Send</Button>
-            {/* Icon buttons  */}
-            <IconButton aria-label='send' color="success" size="small">
-                <SendIcon/>
-            </IconButton>
-        </Stack>
-        <Stack direction="row">
-            <ButtonGroup 
-            variant='outlined' 
-            orientation='vertical' 
-            size='small' 
-            color='secondary' 
-            aria-label='alignment button group'
-            >
-                <Button onClick={()=>alert("Hello, world!")}>Left</Button> 
-                <Button>Center</Button> 
-                <Button>Right</Button>
-            </ButtonGroup>
-             
-        </Stack>
-    </Stack>
-        
-    
-  )
-}
+      <Stack spacing={2} direction="row">
+        {/* Button variants style */}
+        <Button variant="text" href="https://google.com">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
 
-export default MuiButton
+      <Stack spacing={2} direction="row">
+        {/* Button colors style */}
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="text" color="error">
+          Error
+        </Button>
+        <Button variant="outlined" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
+
+      <Stack display="block" spacing={2} direction="row">
+        {/* Button size style */}
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        {/* Button with icons style */}
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          disableRipple
+          onClick={() => alert("HI")}
+        >
+          Send
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />} disableElevation>
+          Send
+        </Button>
+
+        {/* Icon buttons  */}
+        <IconButton aria-label="send" color="success" size="small">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+      <Stack direction="row">
+        {/* Button group alignment and styling  */}
+        <ButtonGroup
+          variant="outlined"
+          orientation="vertical"
+          size="small"
+          color="secondary"
+          aria-label="alignment button group"
+        >
+          <Button onClick={() => alert("Hello, world!")}>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+      {/* Toggle buttons 
+          the toggle button group has the exclusive prop which prevents the group having multiple buttons toggled */}
+      <Stack direction="row">
+        <ToggleButtonGroup
+          aria-label="text formatting"
+          value={formats}
+          onChange={handleFormatChange}
+          size="small"
+          color="success"
+          orientation="vertical"
+          exclusive
+        >
+          <ToggleButton value="bold" aria-label="bold">
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value="italic" aria-label="italic">
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value="underlined" aria-label="underlined">
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MuiButton;
